d96543b496e5031170b99e3afe3e74ba51a1ed5b
BUBU <BUBU@qqqqqqqqqqqqqqqqqqqqqqqqqqqqq.com>
2026-07-17 18:16:08 +0200

    Publish website

--- js/visitor-map.js
+++ js/visitor-map.js
@@ -2,29 +2,8 @@
     var MAP_SRC =
         'https://mapmyvisitors.com/map.js?cl=0e1633&w=a&t=tt&d=Q2xlWwZtn0GJcZ543ENSGeSFiuYOrvksAjWzM4bkNSM&co=0b4975&cmo=3acc3a&cmn=ff5353&ct=cdd4d9';
 
-    function tracker() {
-        return document.getElementById('mapmyvisitors-tracker');
-    }
-
     function holder() {
         return document.getElementById('mapmyvisitors-holder');
-    }
-
-    function loadTrackingScript() {
-        if (document.getElementById('mapmyvisitors')) {
-            return;
-        }
-
-        var mount = tracker();
-        if (!mount) {
-            return;
-        }
-
-        var script = document.createElement('script');
-        script.type = 'text/javascript';
-        script.id = 'mapmyvisitors';
-        script.src = MAP_SRC;
-        mount.appendChild(script);
     }
 
     function resizeWidget() {
@@ -47,35 +26,42 @@
         }
     }
 
-    function showWidgetInPanel() {
-        var widget = document.getElementById('mapmyvisitors-widget');
-        var panelHolder = holder();
-        if (!widget || !panelHolder) {
+    function loadVisitorMap() {
+        var mount = holder();
+        if (!mount) {
             return;
         }
 
-        if (widget.parentElement !== panelHolder) {
-            panelHolder.appendChild(widget);
+        if (document.getElementById('mapmyvisitors')) {
+            resizeWidget();
+            window.setTimeout(resizeWidget, 150);
+            window.setTimeout(resizeWidget, 600);
+            return;
         }
 
-        resizeWidget();
-        window.setTimeout(resizeWidget, 150);
-        window.setTimeout(resizeWidget, 600);
-        window.setTimeout(resizeWidget, 1500);
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.id = 'mapmyvisitors';
+        script.src = MAP_SRC;
+        script.onload = function () {
+            resizeWidget();
+            window.setTimeout(resizeWidget, 150);
+            window.setTimeout(resizeWidget, 600);
+            window.setTimeout(resizeWidget, 1500);
+        };
+        mount.appendChild(script);
     }
 
     function initVisitorMap() {
-        loadTrackingScript();
-
         var panel = document.getElementById('visitorMapPanel');
         if (!panel) {
             return;
         }
 
-        panel.addEventListener('shown.bs.collapse', showWidgetInPanel);
+        panel.addEventListener('shown.bs.collapse', loadVisitorMap);
 
         if (panel.classList.contains('show')) {
-            showWidgetInPanel();
+            loadVisitorMap();
         }
     }
 
